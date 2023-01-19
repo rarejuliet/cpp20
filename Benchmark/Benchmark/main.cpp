0483ec80d5ca0223438f6d5283b89d2563bb4ede
Alpha Juliet <rarejuliet@gmail.com>
2023-01-19 18:33:30 -0500

    Added code to calculate primes and store in a std::map

--- Benchmark/Benchmark/main.cpp
+++ Benchmark/Benchmark/main.cpp
@@ -1,107 +1,87 @@
+#include <array>
+#include <cstddef>
+#include <cstdlib>
+#include <vector>
+#include <string>
 #include <iostream>
-#include <tuple>        // for tie() tuple_cat() swap() and tuple
-#include <cstdlib>      // EXIT_SUCCESS and EXIT_FAILURE
-#include <thread>       // jthread
-int tuple_cat_main(int argc, char **argv, char **env)
-{
-    using namespace std;
-    // Initializing 1st tuple
-    tuple <int, char, float> tup1(20, 'g', 17.5);
+#include <format>
+#include <exception>
+#include <initializer_list>
+#include <cstdint>
+#include <map>
+#include <coroutine>
 
-    // Initializing 2nd tuple
-    tuple <int, char, float> tup2(30, 'f', 10.5);
+template<typename T>
+concept container = requires(T t) {
+    typename T::value_type;
+    typename T::size_type;
+    typename T::allocator_type;
+    typename T::iterator;
+    typename T::const_iterator;
+    t.size();
+    t.begin();
+    t.end();
+    t.cbegin();
+    t.cend();
+};
 
-    // Concatenating 2 tuples to return a new tuple
-    auto tup3 = tuple_cat(tup1, tup2);
 
-    // Displaying new tuple elements
-    cout << "The new tuple elements in order are : ";
-    cout << get<0>(tup3) << " " << get<1>(tup3) << " ";
-    cout << get<2>(tup3) << " " << get<3>(tup3) << " ";
-    cout << get<4>(tup3) << " " << get<5>(tup3) << endl;
-
-    return EXIT_SUCCESS;
+auto add(auto x, auto y) {
+    return x + y;
 }
 
-int tuple_swap_main(int argc, char** argv, char** env)
-{
-    using namespace std;
-    // Initializing 1st tuple
-    tuple <int, char, float> tup1(20, 'g', 17.5);
-
-    // Initializing 2nd tuple
-    tuple <int, char, float> tup2(10, 'f', 15.5);
-
-    // Printing 1st and 2nd tuple before swapping
-    cout << "The first tuple elements before swapping are : ";
-    cout << get<0>(tup1) << " " << get<1>(tup1) << " "
-        << get<2>(tup1) << endl;
-    cout << "The second tuple elements before swapping are : ";
-    cout << get<0>(tup2) << " " << get<1>(tup2) << " "
-        << get<2>(tup2) << endl;
-
-    // Swapping tup1 values with tup2
-    tup1.swap(tup2);
-
-    // Printing 1st and 2nd tuple after swapping
-    cout << "The first tuple elements after swapping are : ";
-    cout << get<0>(tup1) << " " << get<1>(tup1) << " "
-        << get<2>(tup1) << endl;
-    cout << "The second tuple elements after swapping are : ";
-    cout << get<0>(tup2) << " " << get<1>(tup2) << " "
-        << get<2>(tup2) << endl;
-
-    return EXIT_SUCCESS;
-}
-
-int tuple_tie_main(int argc, char** argv, char** env) {
-    using namespace std;
-    // Initializing variables for unpacking
-    int i_val;
-    char ch_val;
-    float f_val;
-
-    // Initializing tuple
-    tuple <int, char, float> tup1(20, 'g', 17.5);
-
-    // Use of tie() without ignore
-    tie(i_val, ch_val, f_val) = tup1;
-
-    // Displaying unpacked tuple elements
-    // without ignore
-    cout << "The unpacked tuple values (without ignore) are : ";
-    cout << i_val << " " << ch_val << " " << f_val;
-    cout << endl;
-
-    // Use of tie() with ignore
-    // ignores char value
-    tie(i_val, ignore, f_val) = tup1;
-
-    // Displaying unpacked tuple elements
-    // with ignore
-    cout << "The unpacked tuple values (with ignore) are : ";
-    cout << i_val << " " << f_val;
-    cout << endl;
-
-    return EXIT_SUCCESS;
-
-}
-
-int main(int argc, char** argv, char** env) {
-    using std::cout;
-    uint32_t n = std::jthread::hardware_concurrency();
-    cout << n << " concurrent threads are supported.\n";
-
-    cout << "main(" << argc << ", ";
-    for (int i = 0; i < argc; ++i) {
-        cout << argv[i] << ", ";
+constexpr bool is_prime(uint64_t n) {
+    for (uint64_t i = 2; i < (n / 2); ++i) {
+        if (n % i == 0) {
+            return false;
+        }
     }
-    for (int i = 0; i < argc; ++i) {
-        cout << env[i];
+    return true;
+}
+
+std::map<uint64_t, bool> calc_next_prime() {
+    bool isprime = false;
+    std::map<uint64_t, bool> primes{
+        {0,true},
+        {1,true},
+        {2,true},
+        {3,true}
+    };
+    for (uint64_t i = 0; i < UINT32_MAX/2; ++i) {
+        isprime = is_prime(i);
+        //std::cout << i << " is prime: " << isprime << std::endl;
+        primes.insert_or_assign(i, isprime);
     }
-    cout << "\n";
-    tuple_cat_main(argc, argv, env);
-    tuple_swap_main(argc, argv, env);
-    tuple_tie_main(argc, argv, env);
+    return primes;
+}
+
+template<class T>
+struct A : std::vector<T> {};
+
+int main(int argc, char* argv[], char** env) {
+    int rval = 0;
+    try {
+        int x{ 3 };
+        float y{ 4.5 };
+        std::cout << std::format("{0} * {1} == {2}\n", x, y, add(x, y));
+        static_assert(true == container<A<int>>);
+        std::array a{ 0,1,2,3,4,5,6,7,8,9 };
+        std::vector<int> v(a.begin(), a.end());
+        static_assert(true == container<std::vector<std::string>>);
+        for (auto& i : v) {
+            std::cout << i << " ";
+        }
+        auto dic = calc_next_prime();
+        std::cout << "Calculated primes...";
+        for (auto& e : dic) {
+            if (e.second)
+                std::cout << std::format("{0} is prime", e.first);
+        }
+        std::cout << "\n";
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
---
+++ containers/tuple1.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <tuple>        // for tie() tuple_cat() swap() and tuple
+#include <cstdlib>      // EXIT_SUCCESS and EXIT_FAILURE
+#include <thread>       // jthread
+int tuple_cat_main(int argc, char **argv, char **env)
+{
+    using namespace std;
+    // Initializing 1st tuple
+    tuple <int, char, float> tup1(20, 'g', 17.5);
+
+    // Initializing 2nd tuple
+    tuple <int, char, float> tup2(30, 'f', 10.5);
+
+    // Concatenating 2 tuples to return a new tuple
+    auto tup3 = tuple_cat(tup1, tup2);
+
+    // Displaying new tuple elements
+    cout << "The new tuple elements in order are : ";
+    cout << get<0>(tup3) << " " << get<1>(tup3) << " ";
+    cout << get<2>(tup3) << " " << get<3>(tup3) << " ";
+    cout << get<4>(tup3) << " " << get<5>(tup3) << endl;
+
+    return EXIT_SUCCESS;
+}
+
+int tuple_swap_main(int argc, char** argv, char** env)
+{
+    using namespace std;
+    // Initializing 1st tuple
+    tuple <int, char, float> tup1(20, 'g', 17.5);
+
+    // Initializing 2nd tuple
+    tuple <int, char, float> tup2(10, 'f', 15.5);
+
+    // Printing 1st and 2nd tuple before swapping
+    cout << "The first tuple elements before swapping are : ";
+    cout << get<0>(tup1) << " " << get<1>(tup1) << " "
+        << get<2>(tup1) << endl;
+    cout << "The second tuple elements before swapping are : ";
+    cout << get<0>(tup2) << " " << get<1>(tup2) << " "
+        << get<2>(tup2) << endl;
+
+    // Swapping tup1 values with tup2
+    tup1.swap(tup2);
+
+    // Printing 1st and 2nd tuple after swapping
+    cout << "The first tuple elements after swapping are : ";
+    cout << get<0>(tup1) << " " << get<1>(tup1) << " "
+        << get<2>(tup1) << endl;
+    cout << "The second tuple elements after swapping are : ";
+    cout << get<0>(tup2) << " " << get<1>(tup2) << " "
+        << get<2>(tup2) << endl;
+
+    return EXIT_SUCCESS;
+}
+
+int tuple_tie_main(int argc, char** argv, char** env) {
+    using namespace std;
+    // Initializing variables for unpacking
+    int i_val;
+    char ch_val;
+    float f_val;
+
+    // Initializing tuple
+    tuple <int, char, float> tup1(20, 'g', 17.5);
+
+    // Use of tie() without ignore
+    tie(i_val, ch_val, f_val) = tup1;
+
+    // Displaying unpacked tuple elements
+    // without ignore
+    cout << "The unpacked tuple values (without ignore) are : ";
+    cout << i_val << " " << ch_val << " " << f_val;
+    cout << endl;
+
+    // Use of tie() with ignore
+    // ignores char value
+    tie(i_val, ignore, f_val) = tup1;
+
+    // Displaying unpacked tuple elements
+    // with ignore
+    cout << "The unpacked tuple values (with ignore) are : ";
+    cout << i_val << " " << f_val;
+    cout << endl;
+
+    return EXIT_SUCCESS;
+
+}
+
+int main(int argc, char** argv, char** env) {
+    using std::cout;
+    uint32_t n = std::jthread::hardware_concurrency();
+    cout << n << " concurrent threads are supported.\n";
+
+    cout << "main(" << argc << ", ";
+    for (int i = 0; i < argc; ++i) {
+        cout << argv[i] << ", ";
+    }
+    for (int i = 0; i < argc; ++i) {
+        cout << env[i];
+    }
+    cout << "\n";
+    tuple_cat_main(argc, argv, env);
+    tuple_swap_main(argc, argv, env);
+    tuple_tie_main(argc, argv, env);
+    return EXIT_SUCCESS;
+}
