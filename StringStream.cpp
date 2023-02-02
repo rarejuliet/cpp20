beaca21878fe79446e85d8522f0a9cecbc432812
Alpha Juliet <rarejuliet@gmail.com>
2023-02-01 21:53:37 -0500

    Lexer can tokenize ids now ( needed before functions will work )

--- StringStream.cpp
+++ StringStream.cpp
@@ -5,11 +5,24 @@
 
 int ss_main(int argc, char* argv[]) {
 	auto s = std::stringstream("78+65-9+(9*3-(3/4))+2");
-	Lexer l(s);
+	auto s2 = std::stringstream("pi/4*3.49-(e^2)");
+	std::string_view sv	("I am a view on this string.");
+	std::cout << sv << std::endl;
+	Lexer l(s2);
 	while(l) {
 		auto tk = l.get_next_token();
 		std::cout << "Got token: " << tk->to_string() << ": " << tk->Buffer
 			<< std::endl;
+		if (tk->Type == TokenType::Eof)
+			break;
+	}
+	Lexer l2(s);
+	while( l2 )	{
+		auto tk = l.get_next_token();
+		std::cout << "Got token: " << tk->to_string() << ": " << tk->Buffer
+			<< std::endl;
+		if (tk->Type == TokenType::Eof)
+			break;
 	}
 	return EXIT_SUCCESS;
 }
--- Token.h
+++ Token.h
@@ -146,6 +146,23 @@ public:
 		double num{ 0 };
 		//buf += ch;
 		switch (ch) {
+		case 'a': case 'b': case 'c': case 'd': case 'e':
+		case 'f': case 'g': case 'h': case 'i': case 'j':
+		case 'k': case 'l': case 'm': case 'n': case 'o':
+		case 'p': case 'q': case 'r': case 's': case 't':
+		case 'u': case 'v': case 'w': case 'x': case 'y':
+		case 'z':
+		case 'A': case 'B': case 'C': case 'D': case 'E':
+		case 'F': case 'G': case 'H': case 'I': case 'J':
+		case 'K': case 'L': case 'M': case 'N': case 'O':
+		case 'P': case 'Q': case 'R': case 'S': case 'T':
+		case 'U': case 'V': case 'W': case 'X': case 'Y':
+		case 'Z':
+			type = TokenType::Id;
+			while(isalnum(stream.peek())) {
+				buf += stream.get();
+			}
+			break;
 			case	'0':
 			case	'1':
 			case	'2':
