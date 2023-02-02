a7a4ebea6fd138108bcb7795b17cc433770c11ba
Alpha Juliet <rarejuliet@gmail.com>
2023-02-01 22:06:09 -0500

    Removed string_view

--- StringStream.cpp
+++ StringStream.cpp
@@ -4,11 +4,12 @@
 #include <iostream>
 
 int ss_main(int argc, char* argv[]) {
+	try
+	{
 	auto s = std::stringstream("78+65-9+(9*3-(3/4))+2");
 	auto s2 = std::stringstream("pi/4*3.49-(e^2)");
-	std::string_view sv	("I am a view on this string.");
-	std::cout << sv << std::endl;
 	Lexer l(s2);
+	Lexer l2(s);
 	while(l) {
 		auto tk = l.get_next_token();
 		std::cout << "Got token: " << tk->to_string() << ": " << tk->Buffer
@@ -16,13 +17,18 @@ int ss_main(int argc, char* argv[]) {
 		if (tk->Type == TokenType::Eof)
 			break;
 	}
-	Lexer l2(s);
+
 	while( l2 )	{
 		auto tk = l.get_next_token();
 		std::cout << "Got token: " << tk->to_string() << ": " << tk->Buffer
 			<< std::endl;
 		if (tk->Type == TokenType::Eof)
 			break;
+	}
+		
+	} catch (std::exception& e) {
+		std::cerr << "Caught a std::exception"
+		std::cerr << e.what() << "\n";
 	}
 	return EXIT_SUCCESS;
 }
