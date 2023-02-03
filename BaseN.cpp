b8586d357ca7b1a1707d25f6a9dd0d788cbee338
ammonezra <ammonchoate@gmail.com>
2023-02-03 13:41:28 -0500

    Added binary to decimal and more conversions
    -Binary to decimal
    -Decimal to binary
    -Hex to decimal
    -Decimal to hex

--- BaseN.cpp
+++ BaseN.cpp
@@ -1,12 +1,13 @@
 #include "BaseN.h"
 
-#include <cstdint>
-#include <string>
+
+
 struct NotImplementedException : std::exception {
 	
 };
 
-inline std::string to_hex(const uint64_t n) {
+//INTEGER TO HEXADECIMAL
+std::string dec_to_hex(const uint64_t n) {
     if (n == 0)
         return "0";
     uint64_t num = n;
@@ -23,11 +24,43 @@ inline std::string to_hex(const uint64_t n) {
     std::reverse(s.begin(), s.end());
     return s;
 }
-
-uint64_t to_int(const std::string& hex) {
-
-	//throw NotImplementedException{};
+//HEXADECIMAL TO DECIMAL
+uint64_t hex_to_dec(const std::string& hex) {
+    std::stringstream ss;
+    uint64_t converted;
+    ss << std::hex << hex;
+    ss >> converted;
+    return converted;
 }
-std::string to_bin(uint64_t n) {
-	throw NotImplementedException{};
+
+//DECIMAL TO BINARY
+std::string dec_to_bin(uint64_t n) {
+    std::string bin;
+    while (n != 0)
+    {
+        bin += (n % 2 == 0 ? '0' : '1');
+        n /= 2;
+    }
+    std::reverse(bin.begin(), bin.end());
+    return bin;
+}
+//BINARY TO DECIMAL
+uint64_t bin_to_dec(std::string bin)
+{
+    uint64_t dec = 0;
+    uint64_t count = 0;
+    for (auto i = bin.rbegin(); i != bin.rend(); ++i) {
+        std::string s; s += (*i); uint64_t a = std::stoul(s);
+        dec += (static_cast<uint64_t>(a) * toThePower(2, count));
+        count += 1;
+    }
+    return --dec;
+}
+
+uint64_t toThePower(uint64_t num, uint64_t power)
+{
+    uint64_t temp = num;
+    for (uint64_t i = 1; i < power; i++)
+        temp = temp * num;
+    return temp;
 }
